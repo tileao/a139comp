b203efa20facf570b0ae7291645c1d7aa108e6e6
tileao <56660742+tileao@users.noreply.github.com>
2026-04-13 08:01:41 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'aw139-companion-root-v4-premium';
+const CACHE_NAME = 'aw139-companion-root-v5-premium';
 const PRECACHE = [
   './',
   './index.html',
@@ -76,7 +76,7 @@ const PRECACHE = [
   './adc/sbvt_chart_p1.png',
   './adc/sbvt_chart_p2.png',
 ];
-const RUNTIME_CACHE = 'aw139-companion-runtime-v4-premium';
+const RUNTIME_CACHE = 'aw139-companion-runtime-v5-premium';
 
 self.addEventListener('install', (event) => {
   self.skipWaiting();
